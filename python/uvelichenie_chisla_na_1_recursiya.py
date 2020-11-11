f879b5518f7018c8c0930fae28d910388f5ba316
Vasyaga <djavito@mail.ru>
2020-11-11 21:42:04 +0500

    dobavil return_digit_v_evlichenie chisla_na_1_recursia

--- python/uvelichenie_chisla_na_1_recursiya.py
+++ python/uvelichenie_chisla_na_1_recursiya.py
@@ -1,16 +1,12 @@
 def reverse(num):
     if num == 0:
-        if num == 0:
-            print()
-            return
-        digit = num % 10
-        print(digit, end="")
-        rest = num // 10
-        reverse(rest)
-    digit = num % 10 + 1
+        print()
+        return
+    digit = (num % 10 + 1) % 10
     rest = num // 10
-    reverse(rest)   
-
-print(reverse(123))
+    return digit
+    reverse(rest)
     
-    
+       
+n = int(input("> "))
+print(reverse(n))
