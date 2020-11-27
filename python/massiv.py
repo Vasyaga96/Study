675fe7f4de4862f492d7724a51f59f659bc87fce
Vasyaga <djavito@mail.ru>
2020-11-27 18:52:42 +0500

    massiv

---
+++ python/massiv.py
@@ -0,0 +1,13 @@
+n = int(input("Введите число индексов: "))
+arr = []
+sum = 0
+for i in range(n):
+    z = int(input(f"Введите значение для массива в индекс  {i}: "))
+    arr.append(z)
+print()
+print(f"Ваш массив {arr}")
+    
+for i in range(n):    
+    sum = arr[i] + sum
+print()
+print(f"Сумма значений вашего массива {sum}")
