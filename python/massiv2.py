ae53069372f00eabed6e4bbee08b9c95d4365b99
Vasyaga <djavito@mail.ru>
2020-12-01 19:13:47 +0500

    massiv2

--- python/massiv2.py
+++ python/massiv2.py
@@ -3,11 +3,11 @@ n = int(input("Введите значение для массива arr: "))
 arr.append(n)
 num = 1
 composition = 1
+z = 1
 while n != 0:
     n = int(input("Введите значение для массива arr: "))
     num += 1
-    if n > 0:
-        arr.append(n)
+    arr.append(num)
 num -=1
 for i in range(num):
     print(f"{i}. {arr[i]}")
