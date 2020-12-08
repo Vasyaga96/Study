f80260b36f9a35dba96db4388206ba9847da9461
Vasyaga <djavito@mail.ru>
2020-12-08 17:12:33 +0500

    massiv3

--- python/massiv3.py
+++ python/massiv3.py
@@ -6,7 +6,6 @@ n = int(input("Введите целое положительное число: 
 for i in range(n):
     x = random.randint(0, 10)
     arr.append(x)
-print(arr)
 for i in range(len(arr)):
     print(f"{i}. {arr[i]}")
 z = int(input("Введите целое положительное число: "))
@@ -14,10 +13,8 @@ for i in range(len(arr)):
     if arr[i] == z:
         k += 1
         otvet = i
-
+        print(f"Ваш индекс {otvet}")
+        break
 if k == 0:
     print("Такого числа нет")
-else:
-    print(f"Ваш ответ {otvet}")
-        
-
+    
