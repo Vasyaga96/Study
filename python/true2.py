a95aa5a7fbdd82aaca798deabff8b7274ee69c40
Vasyaga <djavito@mail.ru>
2020-12-15 00:08:59 +0500

    true

--- python/true2.py
+++ python/true2.py
@@ -1,19 +1,19 @@
-soso = True
-bad = True
+excelent = True
+usual = True
 art = []
 for i in range(5):
     art.append(int(input("Введите оценку: ")))
 for i in range(len(art)):
     print(f"{i}, {art[i]}")
 for i in range(len(art)):
-    if art[i] == 5:
-        soso = False
-    elif art[i] < 3:
-        bad = False
-if soso:
-    print("обычный")
-elif not bad:
-    print("двоечник")
+    if art[i] < 5:
+        excelent = False
+    if art[i] < 3:
+        usual = False
+        break
+if excelent:
+    print("Отличник")
+elif usual:
+    print("Обычный")
 else:
-    print("отличник")
-
+    print("Двоечник")
