968cbb00caf53e458a5ff5815f084d3f263522ad
Vasyaga <djavito@mail.ru>
2021-01-18 19:37:53 +0500

    function(massiv)

--- python/function(massiv)5.py
+++ python/function(massiv)5.py
@@ -1,6 +1,20 @@
-arr = [4,5,1,3]
-def sortirovka (x,y):
-    x = sorted(x, reverse=y)
+arr = [4,5,1,3,6,8,4,5]
+def sortirovka(x,y):
+    index = 0
+    if y == True:
+        for i in range(len(x) - 1):
+            index = i
+            for j in range(i, len(x)):
+                if x[index] > x[j]:
+                    index = j
+            x[i], x[index] = x[index], x[i]
+    if y == False:
+        for i in range(len(x) - 1):
+            index = i
+            for j in range(i, len(x)):
+                if x[index] < x[j]:
+                    index = j
+            x[i], x[index] = x[index], x[i]
     return x
 print(sortirovka(arr, False))
 print(sortirovka(arr, True))
