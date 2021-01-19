983a0d4c2300c32de87f723dd35b4110233a7fe0
Vasyaga <djavito@mail.ru>
2021-01-19 18:50:44 +0500

    str1

--- python/str1.py
+++ python/str1.py
@@ -1,7 +1,18 @@
 s = input("Введите строку ")
-k = ""
-for i in range(len(s) - 1, -1, -1):
-    k = k + s[i]
-if s == k:
-    print("палиндром")
+# k = ""
+# for i in range(len(s) - 1, -1, -1):
+#     k = k + s[i]
+# if s == k:
+#     print("палиндром")
+i = 0
+j = len(s)
+k = True
+while i < j:
+    i += 1
+    j += 1
+    if j == i:
+        True
+    else:
+        False
+
 
