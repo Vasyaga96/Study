e480dfc018249cfd684f82e04e5a22f1a99dfdcb
Vasyaga <djavito@mail.ru>
2021-02-02 17:54:57 +0500

    numbersinstring

--- python/numbersinstring.py
+++ python/numbersinstring.py
@@ -4,13 +4,18 @@ teens = ["ten", "eleven", "twelwe", "thirteen", "fourteen", "fiveteen", "sixteen
 tens = ["", "", "twenty", "thirty", "fourty", "fivety", "sixty", "seventy", "eighty", "ninety"]
 hundred = "hundred"
 orders = ["", "thousand", "million", "billion"]
-# while n > 0:
-three = n % 1000
-one = three % 10
-ten = three % 100 // 10
-hund = three // 100
-if ten != 1:
-    three_result = ones[hund] + " " + hundred + " " + teens[ten] + " " + ones[one]
-else:
-    three_result = ones[hund] + " " + hundred + " " + teens[one]
-print(three_result)
+result = ""
+num_three = 0
+while n > 0:
+    three = n % 1000
+    one = three % 10
+    ten = three % 100 // 10
+    hund = three // 100
+    if ten != 1:
+        three_result = ones[hund] + " " + hundred + " " + teens[ten] + " " + ones[one]
+    else:
+        three_result = ones[hund] + " " + hundred + " " + teens[one]
+    result += " " + orders[num_three] + " " + three_result
+    num_three += 1
+    n = n // 1000
+print(result)
