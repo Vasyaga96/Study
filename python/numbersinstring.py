ea1cfa37e749953b93b9ae8052ee78209ec22f8e
Vasyaga <djavito@mail.ru>
2021-02-02 17:31:41 +0500

    numberinstring'

--- python/numbersinstring.py
+++ python/numbersinstring.py
@@ -1,15 +1,16 @@
 n = int(input("Введите целое число: "))
 ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-tens = ["", "", "twenty", "thirty", "fourty", "fivety", "sixty", "seventy", "eighty", "ninety"]
 teens = ["ten", "eleven", "twelwe", "thirteen", "fourteen", "fiveteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+tens = ["", "", "twenty", "thirty", "fourty", "fivety", "sixty", "seventy", "eighty", "ninety"]
 hundred = "hundred"
 orders = ["", "thousand", "million", "billion"]
+# while n > 0:
 three = n % 1000
-one_result = three % 10
-teens_result = three % 100 // 10
-tens_result = three // 100
-one_str = ones[one_result] + " "
-teens_str = teens[teens_result] + " "
-tens_str = ones[tens_result] + " " + hundred + " "
-numbers = tens_str + teens_str + one_str + " "
-print(numbers)
+one = three % 10
+ten = three % 100 // 10
+hund = three // 100
+if ten != 1:
+    three_result = ones[hund] + " " + hundred + " " + teens[ten] + " " + ones[one]
+else:
+    three_result = ones[hund] + " " + hundred + " " + teens[one]
+print(three_result)
