19f16d73341f916b99329ab60cde5f9c9a63397b
Vasyaga <djavito@mail.ru>
2021-03-01 20:25:44 +0500

    test

--- python/test.py
+++ python/test.py
@@ -14,11 +14,20 @@ for i in range(len(arr)):
 sum_prev = 0
 sum_cur = 0
 sort = True
-sup = []
+sum_arr = []
+index = []
+for i in range(len(arr)):
+    index.append(i + 1)
+    sum_cur = 0
+    for j in range(len(arr[i])):
+        sum_cur += arr[i][j]
+    sum_arr.append(sum_cur)
 while sort:
     sort = False
-    for i in range(len(arr)):
-        sum_cur = 0
-        for j in range(len(arr[i])):
-            sum_cur += arr[i][j]
-        sup.append(sum_cur)
+    for i in range(len(arr) - 1):
+        if sum_arr[i] < sum_arr[i + 1]:
+            sum_arr[i], sum_arr[i + 1] = sum_arr[i + 1], sum_arr[i]
+            index[i], index[i + 1] = index[i + 1], index[i]
+print(sum_arr)
+print(index)
+
