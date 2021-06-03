b3f1a7268de0cc1c9eb5e936ff616d61b4bfeed2
Vasyaga96 <72919807+Vasyaga96@users.noreply.github.com>
2021-06-03 18:48:28 +0500

    array1

---
+++ с# курсы/array1/array1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace array1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"Введите {i + 1} число массива: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                Console.Write($"{arr[i]}, ");
+            }
+            Console.Write(arr[^1]);
+        }
+    }
+}
