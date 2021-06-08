9f33e59d3c51e9563e4097aa529b3e0fbe0a9e1b
Vasyaga96 <72919807+Vasyaga96@users.noreply.github.com>
2021-06-08 17:27:27 +0500

    array2

--- с# курсы/array2/array2/Program.cs
+++ с# курсы/array2/array2/Program.cs
@@ -6,7 +6,34 @@ namespace array2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите размер массива");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"Введите {i + 1} число массива: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                Console.Write($"{arr[i]}, ");
+            }
+            Console.WriteLine(arr[^1]);
+
+            Console.WriteLine("Введите число которое надо найти: ");
+            int numberSearch = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == numberSearch)
+                {
+                    Console.WriteLine($"Ваше число {numberSearch} в массиве под индексом {i}");
+                    break;
+                }
+            }
         }
     }
 }
