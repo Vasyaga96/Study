aefb63a50f774b4755b8969f25ff64969394ca1c
Vasyaga96 <72919807+Vasyaga96@users.noreply.github.com>
2021-07-15 17:43:17 +0500

    array2
    new commit

--- с# курсы/array2/array2/Program.cs
+++ с# курсы/array2/array2/Program.cs
@@ -4,7 +4,46 @@ namespace array2
 {
     class Program
     {
-        private static int Maximum(int[] arr)
+        /*метод генерации массива случайным образом*/
+        static int[] RandomArr(int size, int startRange, int endRange)
+        {
+            int[] randomArr = new int [size];
+            Random rnd = new Random();
+            for (int i = 0; i < randomArr.Length; i++)
+            {
+                randomArr[i] = rnd.Next(startRange, endRange);
+            }
+            return randomArr;
+        }
+
+
+        /*Метод для ввода чисел в созданный массив размером size*/
+        static int[] Input(int size)
+        {
+            int[] arr = new int[size];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"Введите {i + 1} число массива: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            return arr;
+        }
+        /*Метод поиска индекса минимального числа массива*/
+        private static int Minimum(int[] arr)
+        {
+            int index = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < arr[index])
+                {
+                    index = i;
+                }
+            }
+            return index;
+        }
+            /*Метод поиска индекса максимального числа массива*/
+            private static int Maximum(int[] arr)
         {
             int index = 0;
 
@@ -17,7 +56,7 @@ namespace array2
             }
             return index;
         }
-
+        /*Метод для поиска определенного числа, принимает массив и число которое нужно найти*/
         static int Search(int[] arr, int numberSearch)
         {
             for (int i = 0; i < arr.Length; i++)
@@ -29,28 +68,34 @@ namespace array2
             }
             return -1;
         }
-
-        static void Main(string[] args)
+        /*Метод для печати массива принимает массив*/
+        static void Seal(int[] array)
         {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                Console.Write($"{array[i]}, ");
+            }
+            Console.WriteLine(array[^1]);
+        }
+
+            static void Main(string[] args)
+        {
+
             Console.WriteLine("Введите размер массива");
             int n = Convert.ToInt32(Console.ReadLine());
 
-            int[] arr = new int[n];
+            int[] arr = Input(n);
 
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.Write($"Введите {i + 1} число массива: ");
-                arr[i] = Convert.ToInt32(Console.ReadLine());
-            }
-
-            for (int i = 0; i < arr.Length - 1; i++)
-            {
-                Console.Write($"{arr[i]}, ");
-            }
-            Console.WriteLine(arr[^1]);
+            Seal(arr);
 
             int max = Maximum(arr);
             Console.WriteLine($"Самое большое число в массиве {arr[max]} под инсдексом {max}");
+
+            int min = Minimum(arr);
+            Console.WriteLine($"Самое маленькое число в массиве {arr[min]} под индексом {min}");
+
+            Console.WriteLine(RandomArr(n, 1, 10));
         }
     }
 }
+a
