595f0b6db918071679cfc8b0c2b058bfda33cbb2
Vasyaga96 <72919807+Vasyaga96@users.noreply.github.com>
2021-08-09 18:29:55 +0500

    NumberInString and palindrome

---
+++ с# курсы/NumberInString/NumberInString/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace NumberInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            Console.WriteLine(NumberInString(Convert.ToInt32(Console.ReadLine())));            
+        }
+        static string NumberInString(int number)
+        {
+            string[] ones = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+            string[] teens = { "ten", "eleven", "twelwe", "thirteen", "fourteen", "fiveteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+            string[] tens = {"", "", "twenty", "thirty", "fourty", "fivety", "sixty", "seventy", "eighty", "ninety"};
+            string hundred = "hundred";
+            string[] orders = {"", "thousand", "million", "billion"};
+            string result = " ";
+            int numThree = 0;
+            bool sign = true;
+            if (number == 0)
+            {
+                result = "zero";
+            }
+            else if (number < 0)
+            {
+                number = -number;
+                sign = false;
+            }
+            while (number > 0)
+            {
+                int three = number % 1000;
+                int one = three % 10;
+                int ten = three % 100 / 10;
+                int hund = three / 100;
+                string threeResult;
+                if (ten != 1 & hund >= 1 & ten != 0)
+                {
+                    threeResult = ones[hund] + ' ' + hundred + ' ' + tens[ten] + ' ' + ones[one];
+                }
+                else if (ten == 0 & hund >= 1 & one > 0)
+                {
+                    threeResult = ones[hund] + ' ' + hundred + ' ' + ones[one];
+                }
+                else if (ten != 1 & hund >= 1 & ten == 0 & one < 0)
+                {
+                    threeResult = ones[hund] + ' ' + hundred + ' ' + ones[one];
+                }
+                else if (ten != 1 & hund >= 1 & ten == 0 & one == 0)
+                {
+                    threeResult = ones[hund] + ' ' + hundred;
+                }
+                else if (ten != 1 & hund < 1 & three > 9)
+                {
+                    threeResult = tens[ten] + ' ' + ones[one];
+                }
+                else if (ten == 1 & hund < 1)
+                {
+                    threeResult = teens[one];
+                }
+                else if (ten == 0 & hund == 0)
+                {
+                    threeResult = ones[one];
+                }
+                else
+                {
+                    threeResult = ones[hund] + ' ' + hundred + ' ' + teens[one];
+                }
+                if (three == 0)
+                {
+                    result = threeResult + result;
+                }
+                else if (numThree == 0)
+                {
+                    result = threeResult;
+                }
+                else
+                {
+                    result = threeResult + ' ' + orders[numThree] + ' ' + result;
+                }
+                numThree += 1;
+                number /= 1000;
+            }
+            if (sign = false)
+            {
+                result = "minus" + ' ' + result;
+            }
+            return result;
+        }
+    }
+}
---
+++ с# курсы/Palindrome/String/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace String
+{
+    class Program
+    {
+        static bool Palindrome(string line)
+        {
+            int start = 0;
+            int end = line.Length - 1;
+            while(start <= end)
+            {
+                while (line[start] == ' ')
+                {
+                    start += 1;
+                }
+                while (line[end] == ' ')
+                {
+                    end -= 1;
+                }
+                if(line[start] != line[end])
+                {
+                    return false;
+                }
+                start += 1;
+                end -= 1;
+            }
+            return true;
+
+        }
+        static void SealPalindrome(bool palindrome)
+        {
+            if(palindrome)
+            {
+                Console.WriteLine("Строка является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Строка не палиндром");
+            }    
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string line = Console.ReadLine();
+            SealPalindrome(Palindrome(line));
+        }
+    }
+}
