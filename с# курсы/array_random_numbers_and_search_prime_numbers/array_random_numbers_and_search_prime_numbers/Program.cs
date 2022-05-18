562bfe9fadabc5a72ad26fd08bca1efe5570fbe3
Vasyaga96 <72919807+Vasyaga96@users.noreply.github.com>
2022-05-18 21:54:24 +0500

    вспоминаем

---
+++ с# курсы/array_random_numbers_and_search_prime_numbers/array_random_numbers_and_search_prime_numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace array_random_numbers_and_search_prime_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            Random rnd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(1, 100);
+            }
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                Console.Write($"{arr[i]}, ");
+            }
+            Console.WriteLine(arr[^1]);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 1 || arr[i] == 2 || arr[i] == 3)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+                else if(arr[i] % 2 != 0 && arr[i] % 3 != 0 )
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+        }
+    }
+}
