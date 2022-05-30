671c766147cca71b514b9b1da8980312949db7be
Vasyaga96 <72919807+Vasyaga96@users.noreply.github.com>
2022-05-30 17:17:16 +0500

    30.05

--- с# курсы/array_random_numbers_and_search_prime_numbers/array_random_numbers_and_search_prime_numbers/Program.cs
+++ с# курсы/array_random_numbers_and_search_prime_numbers/array_random_numbers_and_search_prime_numbers/Program.cs
@@ -5,75 +5,87 @@ namespace array_random_numbers_and_search_prime_numbers
 {
     class Program
     {
-        static void Main(string[] args)
+        static int[] GenerationRandomArray(int size)
         {
-            Console.WriteLine("Введите размер массива");
-            int n = Convert.ToInt32(Console.ReadLine());
-
-            bool NumberPrime = true;
-            bool needSort = true;
-            int[] arrRandom = new int[n];
-            List<int> arrPrimeNumbers = new List<int> {};
+            int[] arrRandom = new int[size];
 
             Random rnd = new Random();
             for (int i = 0; i < arrRandom.Length; i++)
             {
                 arrRandom[i] = rnd.Next(1, 100);
             }
-
+            return arrRandom;
+        }
+        static void PrintStatic(int[] arrRandom)
+        {
             for (int i = 0; i < arrRandom.Length - 1; i++)
             {
                 Console.Write($"{arrRandom[i]}, ");
             }
             Console.WriteLine(arrRandom[^1]);
+        }
 
+        static List<int> SearchPrimeNumbers(int[] arrRandom)
+        {
+            bool NumberPrime = true;
+            List<int> arrPrimeNumbers = new List<int> { };
             for (int i = 0; i < arrRandom.Length; i++)
             {
-                for(int j = arrRandom[i] - 1; j > 1; j--)
+                for (int j = (int)Math.Sqrt(arrRandom[i]); j > 1; j--)
                 {
-                    if(arrRandom[i] % j == 0)
+                    NumberPrime = true;
+                    if (arrRandom[i] % j == 0)
                     {
                         NumberPrime = false;
                         break;
                     }
                 }
-                if(NumberPrime == true)
+                if (NumberPrime)
                 {
                     arrPrimeNumbers.Add(arrRandom[i]);
                 }
-                NumberPrime = true;
             }
+            return arrPrimeNumbers;
+        }
 
+        static void PrintDinamic(List<int> arrRandom)
+        {
+            for (int i = 0; i < arrRandom.Count - 1; i++)
+            {
+                Console.Write($"{arrRandom[i]}, ");
+            }
+            Console.WriteLine(arrRandom[^1]);
+        }
+
+        static List<int> Sort(List<int> arr)
+        {
+            bool needSort = true;
             while (needSort)
             {
                 needSort = false;
-                for (int i = 0; i < arrPrimeNumbers.Count - 1; i++)
+                for (int i = 0; i < arr.Count - 1; i++)
                 {
-                    if (arrPrimeNumbers[i] > arrPrimeNumbers[i + 1])
+                    if (arr[i] > arr[i + 1])
                     {
-                        int k = arrPrimeNumbers[i];
-                        arrPrimeNumbers[i] = arrPrimeNumbers[i + 1];
-                        arrPrimeNumbers[i + 1] = k;
+                        int k = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = k;
                         needSort = true;
                     }
-                }
+                }            
             }
+            return arr;
+        }
 
-            for (int i = 0; i < arrPrimeNumbers.Count - 1; i++)
-            {
-                Console.Write($"{arrPrimeNumbers[i]}, ");
-            }
-            Console.Write(arrPrimeNumbers[^1]);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] randomArr = GenerationRandomArray(n);
+            PrintStatic(randomArr);
+            List<int>arrPrimeNumbers = SearchPrimeNumbers(randomArr);
+            Sort(arrPrimeNumbers);
+            PrintDinamic(arrPrimeNumbers);
         }
     }
 }
-
-/*if (arrRandom[i] % j == 0)
-{
-    break;
-}
-else
-{
-    arrPrimeNumbers.Add(arrRandom[i]);
-    break;
-}*/
