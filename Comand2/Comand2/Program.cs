3a6bffb53aba111b4abbbb576e7f9aef459f1556
Vasyaga96 <72919807+Vasyaga96@users.noreply.github.com>
2022-06-12 23:15:00 +0500

    12

--- Comand2/Comand2/Program.cs
+++ Comand2/Comand2/Program.cs
@@ -4,6 +4,72 @@ namespace Comand2
 {
     class Program
     {
+        static int[,] GeneratingRandomScoresInTwoDimencionalArray(int CountCommand, int Countompetitions)
+        {
+            int[,] arr = new int[CountCommand, Countompetitions]; //массив команд и соревнований 
+            Random rnd = new Random();
+            for (int i = 0; i < arr.GetLength(0); i++) //пробежались по командам
+            {
+                for (int j = 0; j < arr.GetLength(1); j++) //пробежались по сорревнованиям
+                {
+                    arr[i, j] = rnd.Next(1, 10); //указав номер команды и номер соревнования заносим баллы рандомно
+                }
+            }
+            return arr;
+        }
+        static int[] CountSumOfPoints(int[,] arr)
+        {
+            int[] arrPoint = new int[arr.GetLength(0)]; //массив для суммы очков каждой команды
+            for (int i = 0; i < arr.GetLength(0); i++) //цикл подсчет суммы очков каждой команды
+            {
+                int sum = 0;
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    sum += arr[i, j];
+                }
+                arrPoint[i] = sum;
+            }
+            return arrPoint;
+        }
+        static int[] NumberComand(int[,] arr)
+        {
+            int[] arrNumber = new int[arr.GetLength(0)]; //массив для номеров команд
+            for (int i = 0; i < arr.GetLength(0); i++) //цикл для создания массива номеров комманд
+            {
+                arrNumber[i] = i;
+            }
+            return arrNumber;
+        }
+
+        static int[] SortTwoArray(int[] numberCommand, int[] point)
+        {
+            bool needSort = true;
+            while (needSort)
+            {
+                needSort = false;
+                for (int i = 0; i < point.Length - 1; i++)
+                {
+                    if (point[i] > point[i + 1])
+                    {
+                        int k = point[i];
+                        point[i] = point[i + 1];
+                        point[i + 1] = k;
+                        int l = numberCommand[i];
+                        numberCommand[i] = numberCommand[i + 1];
+                        numberCommand[i + 1] = l;
+                        needSort = true;
+                    }
+                }
+            }
+            return numberCommand;
+        }
+        static void PrintArrTeamsByTheNumbersOfPointsScored(int[] arr)
+        {              
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"{i + 1} место у команды {arr[i]}");
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Введите колличество комманд");
@@ -12,72 +78,9 @@ namespace Comand2
             Console.WriteLine("Введите колличество соревнований");
             int m = Convert.ToInt32(Console.ReadLine());
 
-            int[,] arr = new int[n, m]; //массив команд и соревнований
+            int[,] arr = GeneratingRandomScoresInTwoDimencionalArray(n, m);
 
-
-            
-            static int[,] TwoDimencionalRandomArr (int[,] arr)
-            {
-                Random rnd = new Random();
-                for (int i = 0; i < arr.GetLength(0); i++) //пробежались по командам
-                {
-                    for (int j = 0; j < arr.GetLength(1); j++) //пробежались по сорревнованиям
-                    {
-                        arr[i, j] = rnd.Next(1, 10); //указав номер команды и номер соревнования заносим баллы рандомно
-                    }
-                }
-                return arr;
-            }
-
-            static int[] Point (int[,] arr)
-            {
-                int[] arrPoint = new int[n]; //массив для суммы очков каждой команды
-                for (int i = 0; i < arr.GetLength(0); i++) //цикл подсчет суммы очков каждой команды
-                {
-                    int sum = 0;
-                    for (int j = 0; j < arr.GetLength(1); j++)
-                    {
-                        sum += arr[i, j];
-                    }
-                    arrPoint[i] = sum;
-                }
-                return arrPoint;
-            }
-
-            static int[] NumberComand(int[] arr)
-            {
-                int[] arrNumber = new int[n]; //массив для номеров команд
-                for (int i = 0; i < arr.GetLength(0); i++) //цикл для создания массива номеров комманд
-                {
-                    arrNumber[i] = i;
-                }
-                return arrNumber;
-            }
-
-            
-            bool needSort = true;
-            while (needSort)
-            {
-                needSort = false;
-                for (int i = 0; i < arrPoint.Length - 1; i++)
-                {
-                    if (arrPoint[i] > arrPoint[i + 1])
-                    {
-                        int k = arrPoint[i];
-                        arrPoint[i] = arrPoint[i + 1];
-                        arrPoint[i + 1] = k;
-                        int l = arrNumber[i];
-                        arrNumber[i] = arrNumber[i + 1];
-                        arrNumber[i + 1] = l;
-                        needSort = true;
-                    }
-                }
-            }
-
-            for(int i = 0; i < arrNumber.Length; i++)
-            {
-                Console.WriteLine($"{i + 1} место у команды {arrNumber[i]}");
-            }
+            PrintArrTeamsByTheNumbersOfPointsScored(SortTwoArray(NumberComand(arr), CountSumOfPoints(arr)));
         }
     }
 }
